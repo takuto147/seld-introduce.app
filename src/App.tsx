79ee56758ef8633254c82fa27b4ced555374d078
takuto147 <motherbasis39@gmail.com>
2024-10-09 06:13:07 +0900

    241009

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+import { Header } from "./Header";
 
 export const App = () => {
-  return (
-    <>
-      <div>hello world</div>
+  const [name, setName] = useState<string>("松下 拓斗")
+  const age: number = 26
+  const [hobbys, setHobbys] = useState<string[]>([])
 
-      <p>test</p>
-    </>
+  useEffect(() => {
+    const currentHobby = ["筋トレ", "漫画を読むこと", "猫と戯れること","ちいかわ"]
+    setHobbys(currentHobby)
+  }, [])
+
+
+  const onChange = (): void => {
+    if (name === "マツシタ タクト") {
+      return (
+        setName("松下 拓斗"))
+    }
+
+    return (
+      setName("マツシタ タクト")
+    )
+  }
+
+  return (
+    <div className="container mx-auto p-4 font-bold">
+      <Header />
+      <p>名前： {name}</p>
+      <p>年齢： {age}</p>
+      <p>趣味：{hobbys.join(", ")}</p>
+      <button onClick={onChange} className="border-gray-200 shadow-md rounded-lg p-3 active:translate-y-1 active:shadow-inner hover:bg-blue-300 mt-3">CHANGE</button>
+    </div>
   );
 };
---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+
+export const Header = () => {
+  return (
+    <div>
+      <header className="bg-green-200 mb-4 p-4 border shadow-md rounded-lg flex justify-between items-center">
+        <h1 className="text-lg font-bold">自己紹介サイト</h1>
+      </header>
+
+    </div>);
+};
